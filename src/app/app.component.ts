5162d54c5b5a7fca9073fbb5aa1cdadef22ba86c
Heet Doshi <doshiheet2872003@gmail.com>
2024-03-20 10:24:33 +0530

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'crickfornt';
+
+  constructor(private router:Router){}
+  goToHome(){
+    this.router.navigate(['home'])
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
+import { LiveComponent } from './pages/live/live.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'home' , component:HomeComponent,
+    title: "Home | Cricekt Informer"},
+  {path:'history' , component : HistoryComponent,
+   title: 'Match History | CricketInformer'},
+  {path:'point-table' , component : PointTableComponent,
+    title : 'Point Table | CricketInformer '},
+  {path:'live', component : LiveComponent,
+  title : 'Live Score | CricketInformer'}
+];
