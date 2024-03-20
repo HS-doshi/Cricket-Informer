60aeb7f26f9cbb4721899e9f2a520363ff251b2d
Heet Doshi <doshiheet2872003@gmail.com>
2024-03-20 13:03:05 +0530

    Api Fetching done

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { MatchCardComponent } from '../../components/match-card/match-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [MatchCardComponent , CommonModule],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
+  styleUrl: './history.component.css',
+  providers:[ApiCallService]
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
 
+  allMatches : any;
+  constructor(private api :ApiCallService){}
+
+  ngOnInit(): void {
+        this.api.getAllMatches().subscribe({
+          next:data=>{
+            this.allMatches = data
+            console.log(data)
+          },
+          error : error=>{
+            console.log(error)
+          }
+        })
+  }
 }
